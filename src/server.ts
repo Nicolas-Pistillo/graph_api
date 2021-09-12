26bd96247d38c1cbc14edf07769efa72f129dac6
nicolas <nicolaspistillo@welivery.com.ar>
2021-09-11 22:55:40 -0300

    add_apollo_server

--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,30 @@ import express from "express";
 import compression from "compression";
 import cors from "cors";
 import colors from 'colors';
-import { graphqlHTTP } from "express-graphql";
 import schema from "./schema";
+import { ApolloServer } from "apollo-server-express";
+import { createServer } from "http";
+
 const app = express();
 const PORT = 2916;
 
 //app.use('*',cors); 
 
+const apolloServer = new ApolloServer({
+    schema,
+    introspection: true
+});
+
+const httpServer = createServer(app);
+
+apolloServer.applyMiddleware({ app });
+
 app.use(compression());
 
-app.listen(PORT,() => {
+httpServer.listen(PORT,() => {
 
     const URL_LOCAL = colors.blue(`http://127.0.0.1:${PORT}/graphql`);
 
-    console.log(colors.white(`Servidor corriendo correctamente en ${URL_LOCAL}`));
+    console.log(colors.white(`Apollo server despegado en ${URL_LOCAL}`));
 
-})
-
-app.use('/',graphqlHTTP({
-    schema,
-    graphiql: true
-}))
+});
